5c7f52253153ab3887d116b3dac476fffe93e56e
Azat Murtazin <murtazin.azat@gmail.com>
2026-07-01 14:03:01 +0500

    user struct and methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod user;
+
+use user::User;
+
 fn main() {
-    println!("Hello, world!");
+    let mut user = User::new("John".to_string(), 30);
+    println!("user = {:}", user);
+
+    user.set_name("Mike".to_string());
+    user.set_age(44);
+
+    println!("user = {:}", user);
 }
